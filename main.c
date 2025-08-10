671146835d47929589943ef1615268d94c2dcd85
LucasNarZ <LucasNarloch123@gmail.com>
2025-08-10 18:04:23 -0300

    using new functions

--- main.c
+++ main.c
@@ -5,18 +5,10 @@
 #include <string.h>
 #include <stdlib.h>
 #include "setup-opengl/setup-opengl.h"
+#include "draw/draw.h"
+#include "physics/physics.h"
 
 #define CIRCLE_SEGMENTS 50
-
-void generateCircleVertices(float* vertices, int segments, float radius, float x, float y){
-    vertices[0] = x;// center x
-    vertices[1] = y; // center y
-    for(int i=0; i<=segments; i++){
-        float angle = 2.0f * 3.1415926f * i / segments;
-        vertices[2+2*i] = cosf(angle)*radius + x; 
-        vertices[3+2*i] = sinf(angle)*radius + y; 
-    }
-}
 
 int main(){
     if(!glfwInit()) return -1;
@@ -28,36 +20,31 @@ int main(){
     glfwMakeContextCurrent(window);
     if(glewInit()!=GLEW_OK){printf("GLEW init failed\n");return -1;}
 
-    GLuint shaderProgram=createShaderProgram(vertexShaderSrc,fragmentShaderSrc);
-    glUseProgram(shaderProgram);
+    GLuint vertexShader=glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader,1,&vertexShaderSrc,NULL);
+    glCompileShader(vertexShader);
 
-    int vertexCount = CIRCLE_SEGMENTS+2;
-    float vertices[(CIRCLE_SEGMENTS+2)*2]; // center + segments + repeat first point
+    GLuint fragmentShader=glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader,1,&fragmentShaderSrc,NULL);
+    glCompileShader(fragmentShader);
 
-    generateCircleVertices(vertices,CIRCLE_SEGMENTS,0.1f, 0.00f, 0.00f);
+    GLuint shaderProgram=glCreateProgram();
+    glAttachShader(shaderProgram,vertexShader);
+    glAttachShader(shaderProgram,fragmentShader);
+    glLinkProgram(shaderProgram);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 
-    GLuint VAO,VBO;
-    glGenVertexArrays(1,&VAO);
-    glGenBuffers(1,&VBO);
 
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER,VBO);
-    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
-    glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,2*sizeof(float),(void*)0);
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER,0);
-    glBindVertexArray(0);
+    float gravity = -0.1f;
 
-    float transformedVertices[(CIRCLE_SEGMENTS+2)*2];
-
-    float gravity = -0.9f;
-
-    float posX = 0.0f, posY = 0.0f; // posição atual da bola
-    float velX = 0.0f, velY = 0.0f; // velocidade atual da bola (unidades por segundo)
-                                    //
     float lastFrame = 0.00f;
     float deltaTime = 0.00f;
 
+    Shape *circle = createShape(CIRCLE_SEGMENTS + 2, GL_TRIANGLE_FAN);
+    Body *circleBody = createBody(10.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.2f, 0.2f, 0.0f, 1, circle);
+    Shape *rect = createShape(4, GL_TRIANGLE_FAN);
+    Body *rectBody = createBody(10.0f, 0.2f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.1f, 0.4f, 3.1415f / 2, 0, rect);
     while(!glfwWindowShouldClose(window)){
         float currentFrame = (float)glfwGetTime();
         deltaTime = currentFrame - lastFrame;
@@ -68,32 +55,22 @@ int main(){
 
         glUseProgram(shaderProgram);
 
-        velY += gravity * deltaTime;
-        
-        posX += velX * deltaTime; 
-        posY += velY * deltaTime; 
-                                  
-        if(posY <= -1 + 0.1f){
-            velY = -velY - 0.1f;
-            posY = -1 + 0.1f;
-        }    
-    
+        updatePhysics(circleBody, deltaTime, gravity);
+        updatePhysics(rectBody, deltaTime, gravity);
 
-        generateCircleVertices(transformedVertices, CIRCLE_SEGMENTS, 0.1f, posX, posY);
+        generateCircleVertices(circle->vertices, CIRCLE_SEGMENTS, 0.1f, circleBody->posX, circleBody->posY);
+        updateShapeVertices(circle);
+        drawShape(circle, shaderProgram);
 
-        glBindBuffer(GL_ARRAY_BUFFER,VBO);
-        glBufferSubData(GL_ARRAY_BUFFER,0,sizeof(vertices), transformedVertices);
-        glBindBuffer(GL_ARRAY_BUFFER,0);
-
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLE_FAN,0,vertexCount);
+        generateRectangleVertices(rect->vertices, rectBody->posX, rectBody->posY, rectBody->width, rectBody->height, rectBody->angle);
+        updateShapeVertices(rect);
+        drawShape(rect, shaderProgram);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    glDeleteVertexArrays(1,&VAO);
-    glDeleteBuffers(1,&VBO);
+    cleanupShape(circle);
     glDeleteProgram(shaderProgram);
     glfwDestroyWindow(window);
     glfwTerminate();
